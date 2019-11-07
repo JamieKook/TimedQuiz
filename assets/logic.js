bb8dfe6ed642973b12e43e424bac3d351143c728
Jamie Kook <kookjc6@gmail.com>
2019-11-06 22:21:17 -0600

    added functionality to change questions if an answer selection is clicked

--- assets/logic.js
+++ assets/logic.js
@@ -8,11 +8,13 @@ function countDown(){
        timeOnPage.textContent= "Time: "+ time + "s"; 
        time--; 
 
-    }, 1000)
-
-    if (time === 0) {
+       if (time === 0) {
         clearInterval(timer); 
     }
+
+    }, 1000)
+
+    
 }
 
 document.getElementById("start").addEventListener("click", countDown);
@@ -47,20 +49,53 @@ const questions = [
     }
 ]
 
-// Question/Answer Function
+// Question/Answer Format
 
 main= document.getElementById("main"); 
 
-function askQuestion(arr){
+let questionNum=0; 
+
+
+
+
+
+function generateQuestion(){
+
     while (main.hasChildNodes()) {  
         main.removeChild(main.firstChild);
-      }
-    // for(i=0; i < arr.length; i++){
-        questionsTitle= document.createElement("h4");
-        questionsTitle.textContent= questions[0].title
-        main.append(questionsTitle); 
+    }
 
+    questionsTitle= document.createElement("h4");
+    questionsTitle.textContent= questions[questionNum].title
+    questionsTitle.setAttribute("class", "p-4 mb-4 m-2");
+     
+    main.appendChild(questionsTitle); 
+    main.setAttribute("class", "container row p-5 mx-auto my-5 w-75 bg-light text-center"); 
+
+    for (i=0; i < 3; i++) {
+        let questionAnswer =  document.createElement("button"); 
+        questionAnswer.textContent= questions[questionNum].choices[i];
+        questionAnswer.setAttribute("class", "answerChoice btn btn-primary col-12 col-md-3 my-2 m-md-auto p-2");
+        questionAnswer.setAttribute("id", "question:"+questionNum + " answer:"+i); 
+        if (questionNum !== (questions.length -1)) {
+            questionAnswer.addEventListener("click", generateQuestion);
+        }  
+        main.appendChild(questionAnswer); 
+    }
+
+    questionNum++; 
+  
 
 }
 
-askQuestion(questions); 
+
+
+document.getElementById("start").addEventListener("click", generateQuestion);
+
+
+
+//What happens when you answer the quesiton 
+
+
+
+
