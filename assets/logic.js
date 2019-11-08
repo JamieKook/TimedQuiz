b4198f6de91451d5ad300c92a142b46c6c6f5a10
Jamie Kook <kookjc6@gmail.com>
2019-11-08 10:14:06 -0600

    remove correct/incorrect answer display on final score page when click input box

--- assets/logic.js
+++ assets/logic.js
@@ -175,6 +175,10 @@ function generateQuestion(){
 
 //ending page of quiz with score
 
+function removeCorrect() {
+        main.removeChild(main.firstChild);
+}
+
 function endOfQuiz() {
 
     isTimeStop = true; 
@@ -211,7 +215,8 @@ function endOfQuiz() {
 
     scoreFormInputEl = document.createElement("input"); 
     scoreFormInputEl.setAttribute("type", "text");
-    scoreFormInputEl.setAttribute("class", "form-control col-sm-3 p-1 mb-1"); 
+    scoreFormInputEl.setAttribute("class", "form-control col-sm-3 p-1 mb-1");
+    scoreFormInputEl.addEventListener("click", removeCorrect);  
     scoreFormDividerEl.appendChild(scoreFormInputEl); 
 
     scoreFormSubmit = document.createElement("button"); 
@@ -226,25 +231,6 @@ function endOfQuiz() {
 
     
 
-
-
-//     <form>
-//   <div class="form-group">
-//     <label for="exampleInputEmail1">Email address</label>
-//     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-//     <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-//   </div>
-//   <div class="form-group">
-//     <label for="exampleInputPassword1">Password</label>
-//     <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-//   </div>
-//   <div class="form-check">
-//     <input type="checkbox" class="form-check-input" id="exampleCheck1">
-//     <label class="form-check-label" for="exampleCheck1">Check me out</label>
-//   </div>
-//   <button type="submit" class="btn btn-primary">Submit</button>
-// </form>
-// }
 
 // No longer need this function now that correctanswer is before generate quiz question
 // function updateScore() {
