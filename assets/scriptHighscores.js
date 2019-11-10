d064717d620067391d83cefa641ccda2c8284b0a
Jamie Kook <kookjc6@gmail.com>
2019-11-10 17:39:15 -0600

    limited high scores to the top five scores in order of highest to lowest

--- assets/scriptHighscores.js
+++ assets/scriptHighscores.js
@@ -30,19 +30,61 @@ function clearButtonFxn() {
 clearButton.addEventListener("click", clearButtonFxn); 
 
 
+
 function populateHighScore () {
-
-        let currentLength = storedHighScores.length; 
-        for (i=0 ; i < currentLength; i++) {
-
-            let newScore= document.createElement("li");
-            newScore.setAttribute("class", "bg-warning text-dark m-auto p-2 w-75 my-2");
-            newScore.textContent= ""+storedHighScores[i].initals+" - "+storedHighScores[i].score;
-            scoreListEl.appendChild(newScore); 
-        }
-        
-        
+  
+    let largestScore; 
+    let highScores = []; 
+    let scoreArr = [];
+    let numberOfHighScores= 0; 
+    let currentLength = storedHighScores.length; 
+    for (i=0 ; i < currentLength; i++) {
+        scoreArr.push(storedHighScores[i].score); 
     }
-
+    console.log(scoreArr); 
+    
+   
+    
+    while (numberOfHighScores < 5 && numberOfHighScores < storedHighScores.length) {
+        largestScore= Math.max(...scoreArr); 
+        console.log(largestScore); 
+        for (i=0 ; i < currentLength; i++) {
+            if (storedHighScores[i].score === largestScore && numberOfHighScores < 5) {
+                highScores.push(storedHighScores[i])
+                let spliceIndex= scoreArr.indexOf(largestScore); 
+                scoreArr.splice(spliceIndex,1); 
+                console.log(scoreArr); 
+                console.log(highScores); 
+                numberOfHighScores++; 
+            } 
+        } 
+    }
+  
+    for (i=0 ; i < highScores.length; i++) {
+        let newScore= document.createElement("li");
+        newScore.setAttribute("class", "bg-warning text-dark m-auto p-2 w-75 my-2");
+        newScore.textContent= ""+highScores[i].initals+" - "+highScores[i].score;
+        scoreListEl.appendChild(newScore); 
+    }
+}; 
+    
+        
+    
+    // function populateHighScore () {
+    //     let scoreArr = [];
+    //     let currentLength = storedHighScores.length; 
+    //     for (i=0 ; i < currentLength; i++) {
+    
+    //         scoreArr.push(storedHighScores[i].score);
+    //         console.log(scoreArr); 
+    
+    //         let newScore= document.createElement("li");
+    //         newScore.setAttribute("class", "bg-warning text-dark m-auto p-2 w-75 my-2");
+    //         newScore.textContent= ""+storedHighScores[i].initals+" - "+storedHighScores[i].score;
+    //         scoreListEl.appendChild(newScore); 
+    //     }
+        
+            
+    //     }
     
 
